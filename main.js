1abcb186961c9ba975bd8f273e977198f1940481
Volker Sengler <git@sengler.de>
2022-10-27 08:06:13 +0200

    fix regex

--- main.js
+++ main.js
@@ -68,9 +68,9 @@ let minChargeSeconds     = 0;      // minimum of charge time even when surplus i
 let minRegardSeconds     = 0;      // maximum time to accept regard when charging
 const voltage            = 230;    // calculate with european standard voltage of 230V
 const firmwareUrl        = "https://www.keba.com/de/emobility/service-support/downloads/Downloads";
-const regexP30cSeries    = /<h3 .*class="headline *tw-h3 ">(?:(?:\s|\n|\r)*?)Updates KeContact P30 a-\/b-\/c-\/e-series((?:.|\n|\r)*?)<\/h3/gi;
-//const regexP30xSeries    = /<h3 .*class="headline *tw-h3 ">(?:(?:\s|\n|\r)*?)Updates KeContact P30 x-series((?:.|\n|\r)*?)<\/h3/gi;
-const regexFirmware      = /<div class="mt-3">Firmware-Update\s+((?:.)*?)<\/div>/gi;
+const regexP30cSeries    = /<h3 .*class="headline *tw-h3 ">(?:(?:\s|\n|\r)*?)Updates KeContact P30 a-\/b-\/c-\/e-series((?:.|\n|\r)*?)<h3/gi;
+//const regexP30xSeries    = /<h3 .*class="headline *tw-h3 ">(?:(?:\s|\n|\r)*?)Updates KeContact P30 x-series((?:.|\n|\r)*?)<h3/gi;
+const regexFirmware      = /<div class="mt-3">Firmware Update\s+((?:.)*?)<\/div>/gi;
 const regexCurrFirmware  = /P30 v\s+((?:.)*?)\s+\(/gi;
 
 const stateWallboxEnabled      = "enableUser";                  /*Enable User*/

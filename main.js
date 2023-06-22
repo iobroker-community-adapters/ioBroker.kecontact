0ff5ac5a4fc1ede7fb2b3806303c6d99769eacbb
Volker Sengler <git@sengler.de>
2023-06-22 08:39:37 +0200

    Bugfix

--- main.js
+++ main.js
@@ -1212,7 +1212,7 @@ function isReducedChargingBecause1p3p() {
 function get1p3pPhases() {
     if (isReducedChargingBecause1p3p()) {
         const phases = getStateDefault0(stateChargingPhases);
-        if (isVehicleCharging() && phases > 1) {
+        if (isVehicleCharging() && phases > 1 && getChargingPhaseCount() > 1) {
             adapter.log.error("Charging with " + phases + " but reduced (1p) expected, disabling 1p/3p switch for this charging session");
             reset1p3pSwitching();
             stepFor1p3pSwitching = -1;
@@ -1273,7 +1273,7 @@ function getChargingPhaseCount() {
 }
 
 function isVehicleCharging() {
-    return getWallboxPowerInWatts() > 100 ;
+    return getWallboxPowerInWatts() > 1000 ;
 }
 
 function isVehiclePlugged(myValue) {
